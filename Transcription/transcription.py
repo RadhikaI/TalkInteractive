84df1e7142f6bdeb12bcb0d9c06fd75faa048de9
grace <grace.ku.white@gmail.com>
2025-02-27 21:11:11 +0000

    Cleaned transcription.py, and starting to implement exporting class

--- Transcription/transcription.py
+++ Transcription/transcription.py
@@ -1,6 +1,5 @@
 import whisper
 import subprocess
-# from pydub import AudioSegment
 import os
 import queue
 import time
@@ -17,6 +16,8 @@ class FileException(Exception):
 
 class InvalidURL(Exception):
     pass
+
+
 
 def remove_trailing_punc(transcript):
     i = 1
@@ -36,48 +37,78 @@ def remove_trailing_punc(transcript):
 
 
 
+class TranscriptExporter:
+    def __init__(self, starting_id: int = 0, chunk_path: str = "transcript_chunks.json", whole_path: str = "transcript_whole.json"):
+        self.__id = starting_id
+        self.__chunk_path = chunk_path
+        self.__whole_path = whole_path
 
-class TranscriptionRefiner:
+
+    # TODO: make ids unique if have restarted
+    def update_chunks(self, chunk: str):
+        data = []
+        
+        if os.path.exists(self.__chunk_path):
+            with open(self.__chunk_path, "r") as f:
+                data = json.load(f)
+
+        data.append({"id": self.__id, "chunk": chunk})
+        self.__id += 1
+
+        with open(self.__chunk_path, "w") as f:
+            json.dump(data, f, indent=4)
 
 
-    def __init__(self, transcription_overlap: int = 15):
+    def update_whole_transcript(self, extra_transcript):
+        data = ""
+        
+        if os.path.exists(self.__whole_path):
+            with open(self.__whole_path, "r") as f:
+                data = json.load(f)
+
+        data += extra_transcript
+
+        with open(self.__whole_path, "w") as f:
+            json.dump(data, f)
+
+
+    # restart all files
+    def delete_records(self):
+        pass
+
+
+
+class TranscriptProcessor:
+    
+    def __init__(self):
         self.__raw_transcripts = []
         self.__final_transcripts = []
         self.__whole_transcript = ""
         self.__removed = ""
-        self.__overlap = transcription_overlap
         self.__overlapping = []
-        self.__id = 0
 
 
-    def new_transcript(self, transcript):
+    def new_transcript(self, transcript: str, overlap: int = 200):
         self.__raw_transcripts.append(transcript)
+
         if self.__final_transcripts:
             text2 = self.__merge_transcripts(transcript)
         else:
             text2 = transcript
 
         final, removed = remove_trailing_punc(text2)
-
+        self.__removed = removed
         self.__final_transcripts.append(final)
 
-        #TODO: add overlap of self.__overlap many words from the end of self.__whole_transcript, then export to JSON file (update singular JSON each time with new record for each transcript chunk), also update an whole transcript in a JSON or part of teh same
+        chunk = self.__whole_transcript[-overlap:] + final
 
-        overlapping = self.__whole_transcript[-150:] + final
-        self.__overlapping.append(overlapping)
         self.__whole_transcript += final
-        self.__removed = removed
 
-        self.update_json(overlapping)
+        self.__overlapping.append(chunk)
+        print('chunk', chunk)
+        print('final', final)
 
-
-        print(self.__overlapping)
-    
-
-    def update_json(self, new_chunk, json_file: str = "transcript_chunks.json"):
-        pass
-
-        
+        return chunk, final
 
 
     def clean_text(self, text):
@@ -85,12 +116,10 @@ class TranscriptionRefiner:
 
 
     def __merge_transcripts(self, transcript):
-
         common_letters = self.__find_overlap(self.__final_transcripts[-1], transcript)
 
         if common_letters:
             transcript = self.__remove_overlap(transcript, common_letters)
-        
         
         i = 0
         while i < len(transcript) and not transcript[i].isalpha():
@@ -98,18 +127,10 @@ class TranscriptionRefiner:
         
         if i < len(transcript):
             if transcript[i].isupper():
-                self.__whole_transcript += self.__removed
-
+                transcript = self.__removed + transcript
 
         return transcript
 
-
-    # def __merge(self, text1, text2):
-    #     common_letters = self.__find_overlap(text1, text2)
-    #     if common_letters:
-    #         return self.__remove_overlap(text2, common_letters)
-
-    #     return text2
 
     def __find_overlap(self, text1, text2):
 
@@ -127,6 +148,7 @@ class TranscriptionRefiner:
                 if clean0[len(clean0) - i + word_i] != clean1[word_i]:
                     matches = False
                     break
+
             if matches:
                 max_found = i
 
@@ -145,70 +167,17 @@ class TranscriptionRefiner:
 
         return transcript[i:]
 
- 
-    def testing(self):
-        
-
-
-
-        # Example transcriptions
-        transcriptions = [
-            "who lost three of her limbs, and then there was that little girl, Hind Rajab, a five-year-old Palestinian girl in the Gaza Strip, in a car that had been hit by Israeli missiles as part of this war. All of her relatives in that car were dead. She made a call to an ambulance crew to try and get help. She was in there for days trying to get help and they couldn't get to her, and she died in that situation. Five years of age. Appalling. And as the last, like I said, as the last breath left her body, she experienced that the same way any child who's losing the last breath from their body would experience it. You're not Palestinian or Israeli or Jewish or Muslim.",
-            "Jewish or Muslim in those situations. You're a beautiful child taking your last breath. And can we can we try and get to a point where that is where we stand when we talk about this rather than. Yeah, but Israel, yeah, but Palestine, yeah, but Hamas, yeah, but this, yeah, but that. We can have discussions about how we view the actions of Hamas and the Israeli government. I've had many. I'm having one today with you. I think Hamas are unspeakable. I think the Israeli this Israeli government current Israeli government has made dreadful mistakes in the course of this war, but it didn't pick this particular fight. It did not pick this fight. And I talked about the distinctions. I make no distinction between. and do nothing if no clear overlap"
-        ]
-
-
-        transcriptions1 = ["They are propaganda experts and they really understand how to, how to create fear and how to create anger. That's, that's what they're, that's what they're focusing on. And you can speculate on why it's important to them that they create fear and anger. Maybe it's to create a very severe response that they can then propagandize as well. I don't know. Well, it's pretty clear. No, I mean, I suppose who can read their minds, but it's, it's a, it's a, it's a method, it seems to me, it's a method of justifying what they do and a method of controlling the minds of the people who see those videos and try to convince them that there's some justification in it and isn't this sadism marvellous and aren't we great to those dreadful Jews? I mean, that's essentially it, isn't it? Um, around the world? I, I, I, I don't know.", " I, I, I don't know. I don't know who the audience is for those videos as well. Is it other Islamist extremists? Is it the people of Gaza? It's hard to say. It's so, it feels to me, and I speak personally, it feels so alien watching these watching these, you know, the way that these videos are produced and what they and the audience that, you know, it almost boggles my mind, you know, you see them standing with the coppins of babies today with the guns and the masks. And, and I, you know, it's unspeakable and you can imagine, you can probably imagine. And describing them as, and describing those babies as having been arrested. I mean, it's, it's, it's."]
-
-
-        transcriptions2 = [" In the minds of some people, that's the bigger crime and they will not be moved from that position. Well, I think, frankly, those people are wrong. You know, those people weren't living in the in the settlements, in the disputed areas, not that that would justify it, but they were living in the Israel as it was defined by the when it was voted as a in as a country by most of the world, by the United Nations in 1948. They were doing nothing wrong. They were doing nothing wrong. They weren't. I mean, our dead lift, shits, for example, who who's, you know, death has been announced today, his body was recovered. He was a peace activist from he's an 84 year old peace activist. He used to spend his he spent his entire life in the pursuit of peace.", " He used to go to Gaza to be part of negotiations. He was he used to even up to I think a few weeks before the 7th of October, he used to go on a Friday afternoon of ferry people who needed health care out of Gaza into Israeli hospitals. That was that was the kind of person he was with his wife, you have it. They they weren't. They weren't sir. It's in a way it feels it feels wrong to to say that they were good people are not good people because it wouldn't know. No, no person. It's a war crime. It's a war crime, no matter who the person is. But still, it's a it's it's very important. And for those people, if there are."]
-
-
-        transcriptions3 = [" He used to go to Gaza to be part of negotiations. He was he used to even up to I think a few weeks before the 7th of October, he used to go on a Friday afternoon of ferry people who needed health care out of Gaza into Israeli hospitals. That was that was the kind of person he was with his wife, you have it. They they weren't. They weren't sir. It's in a way it feels it feels wrong to to say that they were good people are not good people because it wouldn't know. No, no person. It's a war crime. It's a war crime, no matter who the person is. But still, it's a it's it's very important. And for those people, if there are.", " It's very important. And for those people, if there are people who feel like there was, you know, those people living in those communities were somehow committing a crime that required them to be, you know, babies to be taken old old men to be taken hostage and then murdered. Just I like them to just think again about that and think whether that's something they, you know, from if that happened to their family, how they would feel. I can almost hear the replies, but I understand the question. Thanks very much, Adam. Adam lawyer's lawyer to some of the British hostage families. Caroline says the terror and suffering of all the victims of war, whether it's from a man with a gun or bombs destroying everything around you is the same. Yeah, point I made almost an hour ago to 49 lbc"]
-
-        # Merge the transcriptions
-        # merged_text = merge_transcriptions(transcriptions)
-        # print(merged_text)
-
-        # need to resolve punctuation
-        t = self.new_transcript(transcriptions[0])
-        t = self.new_transcript(transcriptions[1])
-        self.__final_transcripts = []
-        self.__raw_transcripts = []
-
-        t = self.new_transcript(transcriptions1[0])
-        t = self.new_transcript(transcriptions1[1])
-
-        self.__final_transcripts = []
-        self.__raw_transcripts = []
-
-        t = self.new_transcript(transcriptions2[0])
-        t = self.new_transcript(transcriptions2[1])
-
-        self.__final_transcripts = []
-        self.__raw_transcripts = []
-
-        t = self.new_transcript(transcriptions3[0])
-        t = self.new_transcript(transcriptions3[1])
-        # print(self.__merge(transcriptions[0], transcriptions[1]))
-        # print(self.__merge(transcriptions1[0], transcriptions1[1]))
-        # print(self.__merge(transcriptions2[0], transcriptions2[1]))
-        # print(self.__merge(transcriptions3[0], transcriptions3[1]))
-
-
-        
-
-
 
 
 
 class AudioRecorder:
 
-    def __init__(self, URL: str ="http://media-ice.musicradio.com/LBCUK", model_type: str = "base"):
+    def __init__(self, refiner, exporter, URL: str ="http://media-ice.musicradio.com/LBCUK", model_type: str = "base"):
         self.__URL = URL
 
-        # add parameters
-        self.__Refiner = TranscriptionRefiner()
+        self.__refiner = refiner
+
+        self.__exporter = exporter
 
         self.__to_transcribe = queue.Queue()
 
@@ -222,6 +191,7 @@ class AudioRecorder:
 
         try:
             self.__check_URL()
+
 
         except InvalidURL as e:
             print(str(e))
@@ -257,7 +227,6 @@ class AudioRecorder:
         
 
     def __record_audio_syncronous(self, output_path: str = "./audio-files/temp.aac", duration: int = 30, identifier: str = "temp.aac"):
-        #print('recording')
         if duration <= 0:
             duration = 30
 
@@ -267,7 +236,6 @@ class AudioRecorder:
     
         time.sleep(duration)
         self.__to_transcribe.put(identifier)
-
 
 
 
@@ -328,9 +296,7 @@ class AudioRecorder:
 
 
 
-
-
-    def __conti_transcribe_audio(self, delete_audio: bool = True):
+    def __conti_transcribe_audio(self, delete_audio: bool = True, transcript_overlap: int = 200):
 
         while True:
             if not self.__to_transcribe.empty():
@@ -354,7 +320,10 @@ class AudioRecorder:
                         f.write(transcript)
                     
 
-                    self.__Refiner.new_transcript(transcript)
+                    chunk, extra_transcript = self.__refiner.new_transcript(transcript, transcript_overlap)
+
+                    self.__exporter.update_chunks(chunk)
+                    self.__exporter.update_whole_transcript(extra_transcript)
 
                     if delete_audio:
                         os.remove(WAV_path)
@@ -365,13 +334,13 @@ class AudioRecorder:
 
 
 
-    def start(self, duration: int = 60, overlap: int = 15, delete_audio: bool = True, starting_value: int = 0):
+    def start(self, duration: int = 60, overlap: int = 15, delete_audio: bool = True, starting_value: int = 0, transcript_overlap: int = 200):
         record_thread = threading.Thread(target=self.__conti_record_audio_overlap, args=(duration, overlap, starting_value))
         record_thread.daemon = True  
         record_thread.start()
 
 
-        transcribe_thread = threading.Thread(target=self.__conti_transcribe_audio, args=(delete_audio,))
+        transcribe_thread = threading.Thread(target=self.__conti_transcribe_audio, args=(delete_audio, transcript_overlap))
         transcribe_thread.daemon = True  
         transcribe_thread.start()
 
@@ -380,11 +349,10 @@ class AudioRecorder:
             time.sleep(1)
 
 
+exporter = TranscriptExporter()
+refiner = TranscriptProcessor()
 
+audio = AudioRecorder(refiner=refiner, exporter=exporter, model_type="small")
+audio.start(delete_audio=False, overlap=0.5, duration=45, transcript_overlap=200)
 
-audio = AudioRecorder(model_type="small")
-audio.start(delete_audio=False, overlap=0.5, duration=45)
-
-# t = TranscriptionRefiner()
-# t.testing()
 
